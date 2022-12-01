38e88c82d572107207fa869e57560a77f328f199
Marv J <marvellousojeleke@yahoo.com>
2022-11-30 18:34:57 -0800

    check if number -ve, +ve or zero

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-
-while number:
-    if number > 0:
-        print("{} is positive".format(number))
-        break
-
-    elif number == 0:
-        print("{} is zero".format(number))
-        break
-
-    elif number < 0:
-        print("{} is negative".format(number))
-        break
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is zero".format(number))
+elif number < 0:
+    print("{} is negative".format(number))
