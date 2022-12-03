dbb10b64b882f1a1a0476eeaa13b603517d37fc6
Marv J <marvellousojeleke@yahoo.com>
2022-12-02 19:18:33 -0800

    fix code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-for i in (0, 10):
-    for j in (0, 10):
-        if i == 8 and j == 9:
+for i in range(0, 9):
+    for j in range(1, 10):
+
+        if j > i:
+            if i == 8 and j == 9:
                 print("{:d}{:d}".format(i, j))
-        else:
-            print("{:d}{:d}".format(i, j), end=', ')
+            else:
+                print("{:d}{:d}".format(i, j), end=', ')
