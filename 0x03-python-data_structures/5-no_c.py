98d0797abd6ea2ac03ce97d9144a02cf5a7e0945
Marv J <marvellousojeleke@yahoo.com>
2022-12-08 14:56:22 -0800

    error fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_list = []
+    my_list = ""
     for i in my_string:
         if (my_sting[i] != 'c' or my_string[i] != 'C'):
-            my_string = my_string[] + my_string[i]
-            return my_string
-        return my_string
+            my_list += my_string[i]
+    return my_string
