afc48bae9f569ba7dc40d9292e91b4a9f9dba021
Marv J <marvellousojeleke@yahoo.com>
2022-12-08 15:12:43 -0800

    removes char from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     my_list = ""
     for i in my_string:
-        if (my_sting[i] != 'c' or my_string[i] != 'C'):
-            my_list += my_string[i]
-    return my_string
+        if (i != 'c' and i != 'C'):
+            my_list += i
+    return my_list
