dc9957e34e88208e95c9e6a30d5c9de3a71bdaf3
Marv J <marvellousojeleke@yahoo.com>
2022-12-10 02:55:45 -0800

    returns max number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    maxlist = mylist[0]
+    maxlist = my_list[0]
     for i in my_list:
         if i > maxlist:
             maxlist = i
