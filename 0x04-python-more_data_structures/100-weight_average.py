079adf4ae05ac7414f06a05a338f63584c91e06a
Marv J <marvellousojeleke@yahoo.com>
2022-12-12 05:12:39 -0800

    calculate weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == 0:
+    if my_list == [] or my_list is None:
         return 0
     x = y = 0
     for (score, weight) in my_list:
