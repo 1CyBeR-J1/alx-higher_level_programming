11ec19e1571d938183b31535a0124e4bb2f79a7d
Marv J <marvellousojeleke@yahoo.com>
2022-12-12 05:08:24 -0800

    cal weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,6 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
     for score, weight in my_list:
-        x = sum(score[] * weight[])
-        y = sum(weight[])
+        x = sum(score * weight)
+        y = sum(weight)
         return (x / y)
