0ab0f75febc2251407db2c54d66160c007fa5832
Marv J <marvellousojeleke@yahoo.com>
2022-12-12 04:04:47 -0800

    returnskey with the biggest int

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionry:
+    if a_dictionary:
         max_val = max(a_dictionary.values())
         for key, value in a_dictionary.items():
             if value = max_val:
