dceb2dfa9ed04847ba4c2b105782f63ae24b4a79
Marv J <marvellousojeleke@yahoo.com>
2022-12-12 04:32:20 -0800

    convert roman numeral to ints

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+    roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    ints = list(map(lambda x: roman.get(x, 0), roman_string))
+    length = len(ints)
+    for i in range(length):
+        if i + 1 < length:
+            if ints[i] < ints[i + 1]:
+                ints[i] = -ints[i]
+    return sum(ints)
