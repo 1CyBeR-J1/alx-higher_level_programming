b291277f0e20a4c7b78433728bf6eb52960558ad
Marv J <marvellousojeleke@yahoo.com>
2022-12-12 04:48:57 -0800

    weighted average of all integers tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    my_list = (<score>, <weight>)
+    a = sum(map(lambda x: x[0] * x[1], mylist))
+    b = sum(map(lambda x[1], my_list))
+    return a/b
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,6 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
     roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-
     ints = list(map(lambda x: roman.get(x, 0), roman_string))
     length = len(ints)
     for i in range(length):
