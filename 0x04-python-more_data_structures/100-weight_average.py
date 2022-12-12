8e03b1ee8991e2d22f4bc822785af2e294b5c5d2
Marv J <marvellousojeleke@yahoo.com>
2022-12-12 04:50:29 -0800

    calculate weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,6 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    my_list = (<score>, <weight>)
     a = sum(map(lambda x: x[0] * x[1], mylist))
     b = sum(map(lambda x[1], my_list))
     return a/b
