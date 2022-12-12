e84d5a075e8d266dd837609ba4338801b7af89d1
Marv J <marvellousojeleke@yahoo.com>
2022-12-12 04:53:50 -0800

    returns weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,5 +3,5 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
     a = sum(map(lambda x: x[0] * x[1], mylist))
-    b = sum(map(lambda x[1], my_list))
+    b = sum(map(lambda x: x[1], my_list))
     return a/b
