bdd6b2410d7e6929a731d06258749340dcee17e9
Marv J <marvellousojeleke@yahoo.com>
2023-01-09 13:31:34 -0800

    C functions that print some basic info about Python lists, Python bytes an Python float objects.

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#include <python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <object.h>
+#include <listobject.h>
+#include <byteobject.h>
+#include <floatobject.h>
+#include <string.h>
+
+
+
+/**
+ *
+ */
+
+void print_python_list(PyObject *p);
+
+
+void print_python_bytes(PyObject *p);
+
+
+void print_python_float(PyObject *p);
