12d69a8798c1f6a47c9b3507120076ce8b2300cf
Marv J <marvellousojeleke@yahoo.com>
2023-03-07 14:47:17 -0800

    fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i, c in enumerate(range(122, 96, -1)):
-    print("{:c}".format(c - 32 if i % 2 else c), end="")
+for c in range(122, 96, -1):
+    if (c % 2 != 0):
+        c -= 32
+    print("{}".format(chr(c)), end="")
