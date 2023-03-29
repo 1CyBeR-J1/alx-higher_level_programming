277e809e211a5c5205693f7f5a6fb37ddc09f861
Marv J <marvellousojeleke@yahoo.com>
2023-03-28 19:09:47 -0700

    class Square that defines a square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+
+"""4-square.py - A module that contains a Square class"""
+
+
+class Square:
+    """defines a square by:
+
+    - private instance variable: size
+    - Instantiation with optional size
+    """
+
+    def __init__(self, size=0):
+        """Initialize the instance properties"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrives the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the new value for the size of the square"""
+        if not ininstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size mus be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the area of the square"""
+        return self.__size ** 2
