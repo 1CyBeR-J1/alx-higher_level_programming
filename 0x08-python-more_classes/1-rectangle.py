7b321c2f09cd0fcef9cb4afe6fd6334b71cb425a
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-04-03 06:47:01 -0700

    error fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """setter method for the height attribute"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
