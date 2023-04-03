de733fba0a5f8a797e06f35c406f226890bebc32
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-04-03 09:16:05 -0700

    Add a public class attribute initialized to #

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -56,7 +56,8 @@ class Rectangle:
         """prnts the rectacter with a character #"""
         if self.width == 0 or self.height == 0:
             return ""
-        return '\n'.join('#' * self.__width for i in range(self.__height))
+        return '\n'.join(str(self.print_symbol) * self.__width 
+                for i in range(self.__height))
 
     def __repr__(self):
         """official string representation of the object rectangle"""
