cad080ebb30d9f807894749ccce082be53ff052d
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-04-11 11:07:40 -0700

    Error Fixed

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -21,4 +21,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Return the Rectangle description"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        return f"[self.__class__.__name__] {self.__width}/{self.__height}"
