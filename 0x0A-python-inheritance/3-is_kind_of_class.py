4ec75f8b39825c491c001059ada03390d842f99e
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-04-11 10:37:44 -0700

    Error Fixed

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """A module checks if obj is instance of class"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """returns True if the object is exactly an instance
     of the specified class ; otherwise False."""
     return isinstance(obj, a_class)
