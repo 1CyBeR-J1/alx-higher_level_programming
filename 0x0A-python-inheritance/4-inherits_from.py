1ca23b7ca1bf89be27c983048839299ba8b6cd57
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-04-11 05:00:17 -0700

    check if inherited or not

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that checks if the class inherited a class"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a
+    class that inherited (directly or indirectly) from
+    the specified class ; otherwise False."""
+    return issubclass(type(obj), a_class)
