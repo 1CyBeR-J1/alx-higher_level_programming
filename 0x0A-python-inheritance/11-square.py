ba3aefff7c2233f9ba75e23118783c524cfa56fd
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-04-11 14:39:19 -0700

    Error Fixing

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """A module that creates a Square"""
 
-
 Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
     """Square class inheriting from Rectangle class"""
-
 
     def __init__(self, size):
         """initializing the Square"""
@@ -16,7 +14,7 @@ class Square(Rectangle):
 
     def area(self):
         """Area of the Square"""
-        return self__size ** 2
+        return self.__size ** 2
 
     def __str__(self):
         """return the Square description"""
