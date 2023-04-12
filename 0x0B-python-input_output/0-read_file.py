0321310443bdec25e55ec25afcc6e4f55293bf49
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-04-12 14:38:57 -0700

    Error Fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints to stdout"""
     with open(filename, encoding="utf-8") as Nwfile:
-        for line in Nwfile:
-            print(line, end="")
+        read_data = Nwfile.read()
+        print(read_data)
