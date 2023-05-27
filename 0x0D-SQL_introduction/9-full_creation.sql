51a67012e187cdd6cd7b22fbdb051997751fb15b
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-05-26 19:44:07 -0700

    Mistake Fixed

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -12,7 +12,7 @@
 --	id = 2, name = “Alex”, score = 3
 --	id = 3, name = “Bob”, score = 14
 --	id = 4, name = “George”, score = 8
-CREATE IF NOT EXISTS second_table (id INT, name VARCHAR(256),
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256),
 	score INT)
 INSERT INTO second_table (id, name, score)
 VALUES (1, "John", 10),
