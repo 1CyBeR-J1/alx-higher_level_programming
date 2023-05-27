72ccdf48591d18e8ca10fb01d6f3fc48d2fedce6
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-05-26 20:07:45 -0700

    Mistake Fixed

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- Import in hbtn_0c_0 database this table dump
 -- displays the average temperature (Fahrenheit) by
 -- city ordered by temperature (descending).
-SELECT city, AVG(value) as avg_t
+SELECT city, AVG(value) AS avg_t
 FROM temperatures
 GROUP BY city
-ORDER BY AVG(value) DESC;
+ORDER BY avg_t DESC;
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Import in hbtn_0c_0 database this table dump
 -- displays the top 3 of cities temperature during July
 -- and August ordered by temperature (descending)
-SELECT city, AVG(value) as avg_t
+SELECT city, AVG(value) AS avg_t
 FROM temperatures
 WHERE month = 7 OR month = 8
 GROUP BY city
