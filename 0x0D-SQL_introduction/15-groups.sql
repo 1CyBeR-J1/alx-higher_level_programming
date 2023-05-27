a1e8ac094ff4c334bf1d7b8f52d5e8a703ef6e36
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-05-26 20:03:56 -0700

    Mistake Fixed

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -4,6 +4,6 @@
 --	the number of records for this score with the label number
 -- The list should be sorted by the number of records (descending)
 SELECT score, COUNT(*) AS `number`
-FROM seconf_table
+FROM second_table
 GROUP BY score
 ORDER BY `number` DESC;
