8e2a5e30e3932d244c52550da2d1afd3530f4cad
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-05-28 17:21:14 -0700

    Mistake Fixed

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,5 @@
 -- Results must be sorted in ascending order by cities.id
 SELECT cities.id, cities.name, states.name
 FROM cities INNER JOIN states
-ON states.id = cities.state.id
+ON states.id = cities.state_id
 ORDER BY cities.id;
