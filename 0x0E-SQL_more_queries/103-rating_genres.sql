e4b3e49a588c307b4a8f4e0104dfcd4899d17a99
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-05-28 18:03:49 -0700

    Error Fixed

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,8 +4,8 @@
 -- Results must be sorted in descending order by their rating
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres INNER JOIN tv_show_genres
-ON tv_show_genres.genres_id = tv_genre.id
-INNER JOIN tv_show_rating
+ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_ratings
 ON tv_show_ratings.show_id = tv_show_genres.show_id
 GROUP BY tv_genres.id
 ORDER BY rating DESC;
