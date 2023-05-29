54a73403852a617fc9bb24180024f86322a77c2d
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-05-28 18:01:03 -0700

    Mistake Fixed

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,7 +2,7 @@
 -- Each record should display: tv_shows.title - rating sum
 -- Results must be sorted in descending order by the rating
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows INNER JOIN tv_show_rating
-ON tv_shows.id = tv_show_rating.show_id
+FROM tv_shows INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id
 ORDER BY rating DESC;
