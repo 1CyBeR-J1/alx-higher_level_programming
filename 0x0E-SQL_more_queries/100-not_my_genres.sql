0e17729cc1656f54073742bc8c370c2a28558793
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-05-28 17:43:04 -0700

    Error Fixed

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -10,7 +10,7 @@ ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_genres.name NOT IN
-	(SELECT name
+	(SELECT tv_genres.name
 	FROM tv_genres INNER JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.genre_id
 	INNER JOIN tv_shows
