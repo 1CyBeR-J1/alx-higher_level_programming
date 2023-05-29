4e7fa0aaed6fc796203f18b006a71e2f2d470bd7
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-05-28 17:45:18 -0700

    Error Fixed

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,7 +4,7 @@
 --	title = Dexter (but the id can be different)
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
-SELECT tv_genres.name
+SELECT DISTINCT tv_genres.name
 FROM tv_genres INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
