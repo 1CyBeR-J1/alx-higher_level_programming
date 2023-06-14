be59bb191845855719940de5f1671180664897cd
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-06-13 22:14:14 -0700

    Requirement Failure Fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const numArgv = process.argv.length - 2
 if (numArgv === 0) {
-  console.log('No argument')
+  console.log('No argument');
 } else if (numArgv === 1) {
-  console.log('Argument found')
+  console.log('Argument found');
 } else {
-  console.log('Arguments found')
+  console.log('Arguments found');
 }
