ce5e1fdff6b733b485ff2a7890a54fd680f6fe03
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-06-13 23:01:59 -0700

    semistandard Error Fixed

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,14 +2,14 @@
 const myObject = {
   type: 'object',
   value: 12
-}
+};
 console.log(myObject);
 /*
 YOUR CODE HERE
 */
 myObject.incr = function () {
-  myObject.value++
-}
+  myObject.value++;
+};
 
 myObject.incr();
 console.log(myObject);
