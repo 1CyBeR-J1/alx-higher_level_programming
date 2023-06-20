62c0b5577bc532010f92548ff3e29ed1f34132c8
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2023-06-19 21:25:44 -0700

    Requirement error fixing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,8 +12,7 @@ if __name__ == "__main__":
             passwd=sys.argv[2], db=sys.argv[3],
             port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states
-            ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
 
     for r in rows:
