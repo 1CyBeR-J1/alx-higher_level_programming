8bac0a6a1b64e6a1327061b267d1621150f2864d
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-12-01 23:39:57 -0800

    error in url fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     response = requests.post(url, data=letter)
     try:
         response = r.json()
-        if response = {}:
+        if response == {}:
             print("No result")
         else:
             print("[{}] {}".format(response.get("id"), response.get("name")))
