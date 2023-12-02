79b686f032bfd57c7d45d56cbb310be480e1c7f6
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-12-01 23:30:46 -0800

    fixed error in url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = https://alx-intranet.hbtn.io/status
+    url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
