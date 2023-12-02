f0cacd8cbaeaeb6707a9205b43bebcbe8bc6e85b
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-12-01 23:43:20 -0800

    error in url fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import requests
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    letter = {"q": argv[1][0] if len(argv) > 1 else ""}
+    letter = {"q": sys.argv[1][0] if len(argv) > 1 else ""}
     response = requests.post(url, data=letter)
     try:
         response = r.json()
