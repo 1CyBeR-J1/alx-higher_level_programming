e71c627239729718289f73af7a0003ac952fb10d
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-12-01 23:52:58 -0800

    takes your GitHub credentials to display your id

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import requests
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    letter = {"q": sys.argv[1] if len(argv) > 1 else ""}
+    letter = {"q": sys.argv[1] if len(sys.argv) > 1 else ""}
     response = requests.post(url, data=letter)
     try:
         response = r.json()
