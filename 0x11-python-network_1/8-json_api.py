63df9823b5c7666d5b63349a5192845e7b2f56ba
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-12-01 22:45:19 -0800

    takes letter, sends POST req to url

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = http://0.0.0.0:5000/search_user
+    letter = {"q": argv[1][0] if len(argv) > 1 else ""}
+    response = requests.post(url, data=letter)
+    try:
+        response = r.json()
+        if response = {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
